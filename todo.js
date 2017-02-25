a4c3f11a4bd199ae7adfed9874ac88727086f054
Dave Degeatano <davey.dee33@gmail.com>
2017-02-25 22:56:28 +1100

    v3 complete. using objects and methods

--- todo.js
+++ todo.js
@@ -1,5 +1,36 @@
-// JavaScript watch and code 'ToDo'
+// JavaScript watch and code 'ToDo' -- v3: objects and methods
 
+var todoList = {
+    todos: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+
+    displayTodos: function() {
+        console.log("My Todos: ", this.todos);  // can I use a + here instead of , ?
+    },
+
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+};
+
+
+
+
+
+
+
+
+/* v2 : array and functions
 var todos = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
 
 
@@ -21,3 +52,4 @@ function changeTodo(position, newValue){
 	todos[position] = newValue;
 	displayTodos;
 }
+*/
