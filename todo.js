7e22a3a16a93438be2cfe4d3edc85215b50a2930
Dave Degeatano <davey.dee33@gmail.com>
2017-02-27 22:20:02 +1100

    v6. toggleAll method.  set 'complete' to true or false for all todos

--- todo.js
+++ todo.js
@@ -46,6 +46,29 @@ var todoList = {
         // isn't this the same???  why did he write it in 2 lines, more complicated, if I could write it in one?  https://watchandcode.com/courses/60264/lectures/946811
         //this.todos[position].completed = !this.todos[position].completed;
         this.displayTodos();
+    },
+
+    // if at least 1 is not complete, set that and anything else to complete.
+    // if all are complete already, set them all to not-complete
+    // if all are not-complete, set them to complete
+    toggleAll: function() {
+        var numCompleted = 0;
+        for (var i = 0; i < this.todos.length; i++) {
+            if (this.todos[i].completed === false) {
+                numCompleted++;
+                this.toggleCompleted(i); // i wonder if it's a better idea to call this method, or set it to false explictly?
+                //this.todos[i].completed = true;
+                console.log(i + " was not completed.  setting to complete");
+            }
+        }
+        if (numCompleted === 0){
+            console.log("all were completed, setting all to not-completed");
+            for (var i = 0; i < this.todos.length; i++) {
+                this.toggleCompleted(i);
+                //this.todos[i].completed = false;
+            }
+        }
+        this.displayTodos();
     }
 };
 
